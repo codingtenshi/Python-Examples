c18a085aa872bfa791b7ea8fb1603aad2b0f0da1
kireitensh <karina.gornicka@wp.pl>
2023-01-18 11:29:32 +0100

    "add new README"

---
+++ Exemples of Python/function1.py
@@ -0,0 +1,11 @@
+# Write a function that accepts one string argument
+
+def print_to_terminal(welcome):
+    print(welcome)
+
+print_to_terminal("Hello")
+print_to_terminal ("Kurwaaaa, ja pierdolę")
+
+name = "Karina"
+
+print_to_terminal(f"Hello {name}")
