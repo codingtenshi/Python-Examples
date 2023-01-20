e4247e864bf3b5d41160f873f886a22f62653f92
kireitensh <karina.gornicka@wp.pl>
2023-01-20 15:35:24 +0100

    add count_char_function

---
+++ Exemples of Python/count_char_function.py
@@ -0,0 +1,8 @@
+# Stworzyć funkcję, która zwraca ile jest znaków w stringu
+
+def count_char(str):
+    return len(str)
+
+print(count_char("Ala ma kota"))
+print(count_char("Moja macocha jest kosmitką"))
+print(count_char("Łukasz Górnicki"))
