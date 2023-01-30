f30c32da5039571a5fceab78926703e7be05ecb3
kireitensh <karina.gornicka@wp.pl>
2023-01-30 12:02:15 +0100

    move files to directory

---
+++ Exemples of Python/function3.py
@@ -0,0 +1,9 @@
+# Napisz funkcję, której parametrem jest lista liczb.
+# Zadaniem funkcji jest zsumować co drugi element tej listy, zwrócić simę tych liczb z trzema róznymi danymi wyjściowymi.
+
+def list_sum_up():
+        return{list_sum_up[::2]}
+
+list1 = [1,2,3,4,5]
+
+print(list1[::2])
