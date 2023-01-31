44466765a0c34917b6b12be3ef6195574d224f75
kireitensh <karina.gornicka@wp.pl>
2023-01-31 12:16:44 +0100

    add function that returns sum of even numbers

---
+++ python /function_6_ex.py
@@ -0,0 +1,15 @@
+# napisać funkcję, która za parametr ma listę liczb a zwraca sumę liczb parzystych
+# even number - liczba parzysta
+
+def sum_of_list_even_numbers(numbers):
+    
+    counter = 0 
+    for i in numbers:
+        if i % 2 == 0:
+            counter += i
+    return counter
+    
+    
+result = sum_of_list_even_numbers([3, 6, 8])
+print(result)   
+print(sum_of_list_even_numbers([2, 3,4]))
