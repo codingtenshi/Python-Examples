2e1f72dbc985953a7bfba3397f3731459fd27205
kireitensh <karina.gornicka@wp.pl>
2023-01-31 10:10:16 +0100

    add 'a' counter with loop for

---
+++ python /function_4_ex.py
@@ -0,0 +1,17 @@
+# napisać funkcję, która policzy ile razy występuje litera a w zdaniu
+
+def count_letter_a_in_sentence(str):
+    counter = 0
+    for char in str:
+        if char == "A" or char == "a":
+            counter += 1
+    return counter
+  
+    
+
+
+sentence = "Ala ma kota?"
+sentence_2 ='Why do you add this "aaaaa"?'
+result = count_letter_a_in_sentence(sentence_2)
+print(result)
+print(count_letter_a_in_sentence(sentence))
