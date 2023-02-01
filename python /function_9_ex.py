b315d706627a27cea445e138ff13efaa58c584ce
kireitensh <karina.gornicka@wp.pl>
2023-02-01 14:23:55 +0100

    improvement function that returns backwards str

--- python /function_9_ex.py
+++ python /function_9_ex.py
@@ -1,8 +1,13 @@
 # napisać funkcję, w której string zaisany zostaje w odwrotnej kolejności, mozna go czytać od tyłu
 
 def str_read_backwards(sentence):
+    lenght_counter = len(sentence)
+    reverted = ("")
+    counter = 1
     for i in sentence:
-        return sentence[::-1]
+        reverted += sentence[lenght_counter - counter]
+        counter += 1
+    return reverted
 
 
 inscription = str_read_backwards("Hello world")
