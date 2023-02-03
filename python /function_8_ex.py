b8085263c241939d030439fefa5359a9f611cbb4
kireitensh <karina.gornicka@wp.pl>
2023-02-03 10:18:19 +0100

    add function that returns sum of int in characters

--- python /function_8_ex.py
+++ python /function_8_ex.py
@@ -1,12 +1,26 @@
 # napisać funkcję, która zwraca sumę liczb ze stringa
 
-def return_sum_of_char_in_str(str):
+# def return_sum_of_char_in_str(str):
+#     counter = 0
+#     for i in str:
+#         counter += 1
+#     return counter
+
+# sentence = return_sum_of_char_in_str("Bla, bla, bla")
+# print(sentence)
+# print(type(sentence))
+# print(len("Bla, bla7git status, bla"))
+
+
+def return_sum_of_int_in_char(sentence):
     counter = 0
-    for i in str:
-        counter += 1
+    for i in sentence:
+        if i.isnumeric(): 
+            counter += 1
     return counter
 
-sentence = return_sum_of_char_in_str("Bla, bla, bla")
+
+sentence = return_sum_of_int_in_char(("Bl5a, bla7git status, bla"))
+sentence2 = return_sum_of_int_in_char("223hga552")
+print(sentence2)
 print(sentence)
-print(type(sentence))
-print(len("Bla, bla7, bla"))
