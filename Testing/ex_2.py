99391b2035dd01710cf4676eca52a04670149f4c
kireitensh <karina.gornicka@wp.pl>
2023-02-09 17:51:00 +0100

    error correction

--- Testing/ex_2.py
+++ Testing/ex_2.py
@@ -6,7 +6,7 @@ if argument is not bigger than max_value
 def sum_args(max_value, *args):
     sum_elements = 0
     
-    max_value = max_value if isinstance(max_value, int) else 100:
+    max_value = max_value if isinstance(max_value, int) else 100
     
     for value in args:
         if isinstance(value, int) and value <= max_value:
