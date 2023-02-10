359a944fb0c41f142e44a606a69c127d05fd0472
kireitensh <karina.gornicka@wp.pl>
2023-02-10 16:14:31 +0100

    refactor

--- python /function_4_ex.py
+++ python /function_4_ex.py
@@ -7,11 +7,3 @@ def count_letter_a_in_sentence(str):
             counter += 1
     return counter
   
-    
-
-
-sentence = "Ala ma kota?"
-sentence_2 ='Why do you add this "aaaaa"?'
-result = count_letter_a_in_sentence(sentence_2) # wynik funkcji
-print(result)
-print(count_letter_a_in_sentence(sentence))
