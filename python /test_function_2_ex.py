c0ce58be3198f898cc10754f60207302dc5d8c0f
kireitensh <karina.gornicka@wp.pl>
2023-02-10 11:55:48 +0100

    add test to function2_ex.py

---
+++ python /test_function_2_ex.py
@@ -0,0 +1,12 @@
+from function_2_ex import double_number
+
+def test_doble_number():
+    a = 2
+    expected = 4
+    
+    result = double_number(a)
+    assert result == expected
+    
+
+    
+    
